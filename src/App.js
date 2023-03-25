d9eecc08ebf3be06fa4d92ae33445eb3ebe5664e
Markus Wedler <markuswedler8@gmail.com>
2023-03-25 17:08:40 +1000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <main>
 
-export default App;
+      <div className="container">
+        <h3>Buttons</h3>
+        <div className="flex">
+          <button>Sign up</button>
+          <button className="outline">Sign up</button>
+          <button className="light">Sign up</button>
+        </div>
+        <div className="flex">
+          <button className="small">Sign up</button>
+          <button className="small outline">Sign up</button>
+          <button className="small light">Sign up</button>
+        </div>
+        <div className="flex">
+          <button disabled>Sign up</button>
+          <button className="outline" disabled>Sign up</button>
+          <button className="light" disabled>Sign up</button>
+        </div>
+        <div className="flex">
+          <button className="small" disabled>Sign up</button>
+          <button className="small outline" disabled>Sign up</button>
+          <button className="small light" disabled>Sign up</button>
+        </div>
+      </div>
+
+      <div className="container">
+        <h3>Text Inputs</h3>
+        <div className="flex col">
+          <form className="text-form">
+            <label htmlFor="textInput_01">Text input</label>
+            <input id="textInput_01" type="email" placeholder="Enter your email" />
+          </form>
+          <form>
+            <label htmlFor="textInput_02">Text input</label>
+            <textarea id="textInput_02" placeholder="Enter your email"></textarea>
+          </form>
+        </div>
+      </div>
+
+      <div className="container">
+        <h3>Checkboxes</h3>
+        <form>
+          <input className="checkbox-input" type="checkbox" name="checkboxGroup_01" id="checkboxGroup_checkbox_01" />
+          <label className="checkbox-label" htmlFor="checkboxGroup_checkbox_01">
+            <div className="checkbox"><div/></div>
+            <span>Enter your label text</span>
+          </label>
+          <input className="checkbox-input" type="checkbox" name="checkboxGroup_01" id="checkboxGroup_checkbox_02" />
+          <label className="checkbox-label" htmlFor="checkboxGroup_checkbox_02">
+            <div className="checkbox"><div/></div>
+            <span>Enter your label text</span>
+          </label>
+          <input defaultChecked className="checkbox-input" type="checkbox" name="checkboxGroup_01" id="checkboxGroup_checkbox_03" />
+          <label className="checkbox-label" htmlFor="checkboxGroup_checkbox_03">
+            <div className="checkbox"><div/></div>
+            <span>Enter your label text</span>
+          </label>
+        </form>
+      </div>
+
+    </main>
+  )
+}
