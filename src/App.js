e8555b49af7d7f00ac9a054b723cd3b1edcddc7b
Markus Wedler <markuswedler8@gmail.com>
2023-03-26 01:23:51 +1000

    Added Radio buttons + fixes

--- src/App.js
+++ src/App.js
@@ -6,22 +6,22 @@ export default () => {
         <h3>Buttons</h3>
         <div className="flex">
           <button>Sign up</button>
-          <button className="outline">Sign up</button>
+          <button className="white">Sign up</button>
           <button className="light">Sign up</button>
         </div>
         <div className="flex">
           <button className="small">Sign up</button>
-          <button className="small outline">Sign up</button>
+          <button className="small white">Sign up</button>
           <button className="small light">Sign up</button>
         </div>
         <div className="flex">
           <button disabled>Sign up</button>
-          <button className="outline" disabled>Sign up</button>
+          <button className="white" disabled>Sign up</button>
           <button className="light" disabled>Sign up</button>
         </div>
         <div className="flex">
           <button className="small" disabled>Sign up</button>
-          <button className="small outline" disabled>Sign up</button>
+          <button className="small white" disabled>Sign up</button>
           <button className="small light" disabled>Sign up</button>
         </div>
       </div>
@@ -56,6 +56,27 @@ export default () => {
           <input defaultChecked className="checkbox-input" type="checkbox" name="checkboxGroup_01" id="checkboxGroup_checkbox_03" />
           <label className="checkbox-label" htmlFor="checkboxGroup_checkbox_03">
             <div className="checkbox"><div/></div>
+            <span>Enter your label text</span>
+          </label>
+        </form>
+      </div>
+
+      <div className="container">
+        <h3>Radio Buttons</h3>
+        <form>
+          <input className="radio-input" type="radio" name="radioGroup_01" id="radioGroup_radio_01" />
+          <label className="radio-label" htmlFor="radioGroup_radio_01">
+            <div className="radio" />
+            <span>Enter your label text</span>
+          </label>
+          <input className="radio-input" type="radio" name="radioGroup_01" id="radioGroup_radio_02" />
+          <label className="radio-label" htmlFor="radioGroup_radio_02">
+            <div className="radio" />
+            <span>Enter your label text</span>
+          </label>
+          <input defaultChecked className="radio-input" type="radio" name="radioGroup_01" id="radioGroup_radio_03" />
+          <label className="radio-label" htmlFor="radioGroup_radio_03">
+            <div className="radio" />
             <span>Enter your label text</span>
           </label>
         </form>
