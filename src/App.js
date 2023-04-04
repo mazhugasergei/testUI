a2b9b7278b859f09f4c64f101bef9623cfa3fd73
Markus Wedler <markuswedler8@gmail.com>
2023-04-04 15:15:55 +1000

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,73 @@
 export default () => {
   return (
     <main>
+      <header>
+        <a href="/" className="logo"><h4>Quickly<span>.</span></h4></a>
+        <nav className="menu-cont">
+          <ul className="menu-list">
+            <li className="menu-item"><a href="/">About us</a></li>
+            <li className="menu-item"><a href="/">Products</a></li>
+            <li className="menu-item"><a href="/">Features</a></li>
+            <li className="menu-item"><a href="/">Our Blog</a></li>
+            <li className="menu-item"><a href="/">Downloads</a></li>
+          </ul>
+        </nav>
+        <div className="log-in-cont">
+          <a href="/" className="sign-up btn small light">Sign up</a>
+          <a href="/" className="log-in btn small white">Log In</a>
+        </div>
+      </header>
+
+      <header>
+        <nav className="menu-cont">
+          <ul className="menu-list">
+            <li className="menu-item"><a href="/">About us</a></li>
+            <li className="menu-item"><a href="/">Products</a></li>
+            <li className="menu-item"><a href="/">Features</a></li>
+            <li className="menu-item"><a href="/">Our Blog</a></li>
+            <li className="menu-item"><a href="/">Downloads</a></li>
+          </ul>
+        </nav>
+        <a href="/" className="logo"><h4>Quickly<span>.</span></h4></a>
+        <div className="log-in-cont">
+          <a href="/" className="sign-up btn small light">Sign up</a>
+          <a href="/" className="log-in btn small white">Log In</a>
+        </div>
+      </header>
+
+      <header className="primary">
+        <a href="/" className="logo"><h4>Quickly<span>.</span></h4></a>
+        <nav className="menu-cont">
+          <ul className="menu-list">
+            <li className="menu-item"><a href="/">About us</a></li>
+            <li className="menu-item"><a href="/">Products</a></li>
+            <li className="menu-item"><a href="/">Features</a></li>
+            <li className="menu-item"><a href="/">Our Blog</a></li>
+            <li className="menu-item"><a href="/">Downloads</a></li>
+          </ul>
+        </nav>
+        <div className="log-in-cont">
+          <a href="/" className="sign-up btn small light">Sign up</a>
+          <a href="/" className="log-in btn small white">Log In</a>
+        </div>
+      </header>
+
+      <header className="primary">
+        <nav className="menu-cont">
+          <ul className="menu-list">
+            <li className="menu-item"><a href="/">About us</a></li>
+            <li className="menu-item"><a href="/">Products</a></li>
+            <li className="menu-item"><a href="/">Features</a></li>
+            <li className="menu-item"><a href="/">Our Blog</a></li>
+            <li className="menu-item"><a href="/">Downloads</a></li>
+          </ul>
+        </nav>
+        <a href="/" className="logo"><h4>Quickly<span>.</span></h4></a>
+        <div className="log-in-cont">
+          <a href="/" className="sign-up btn small light">Sign up</a>
+          <a href="/" className="log-in btn small white">Log In</a>
+        </div>
+      </header>
     </main>
   )
 }
